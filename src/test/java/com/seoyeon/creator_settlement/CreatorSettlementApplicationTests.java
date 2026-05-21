7b9529f25a958d628158d1b2d36c0ca24a016360
meisseoyeon <dkssud1191@naver.com>
2026-05-21 22:04:23 +0900

    Initial commit

---
+++ src/test/java/com/seoyeon/creator_settlement/CreatorSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seoyeon.creator_settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorSettlementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
